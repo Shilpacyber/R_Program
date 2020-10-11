cabd1d20372890865ab457623ebc4d3fae3e54fa
Shilpa Kshetri <shilpakshetri1@gmail.com>
2020-10-10 20:44:30 -0500

    first commit

---
+++ Walmart.R
@@ -0,0 +1,10 @@
+install.packages('gtrendsR',dependencies = T)
+library('gtrendsR')
+
+walmart=gtrends("walmart")
+
+plot(gtrendsR::gtrends(keyword = "walmart",geo="US", time="2014-01-01 2019-12-31"))
+plot (walmart$interest_over_time$date, walmart$interest_over_time$hits,type= "l")
+
+
+
