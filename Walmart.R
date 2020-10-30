d66e445ba5aff418a8a1a279a51c9dea71f50d51
anuragdhakal <anuragw2@gmail.com>
2020-10-29 20:39:17 -0400

    commit 2

--- Walmart.R
+++ Walmart.R
@@ -6,5 +6,6 @@ walmart=gtrends("walmart")
 plot(gtrendsR::gtrends(keyword = "walmart",geo="US", time="2014-01-01 2019-12-31"))
 plot (walmart$interest_over_time$date, walmart$interest_over_time$hits,type= "l")
 //comment 1
+//comment 2
 
 
